58828f70f88ad7f4e681782c2ebeaefb9affb4ac
dadlez <lukaszdadlez@gmail.com>
2017-08-21 20:28:50 +0200

    working on running timer in nested loops sequence

--- src/components/sets/Timer.jsx
+++ src/components/sets/Timer.jsx
@@ -6,44 +6,13 @@ class Timer extends React.Component {
 		super(props);
 		this.state = {
 			minutes: this.props.minutes,
-			seconds: this.props.seconds,
-			edit: this.props.edit,
-			active: false,
-			run: this.props.run
-		}
-	}
-
-	startInterval() {
-		let timerInterval = setInterval(() => {
-			console.log(this.state.seconds);
-			if (this.state.seconds == 0 && this.state.minutes == 0) {
-				clearInterval(this.timerInterval);
-			} else if (this.state.seconds < 1) {
-				this.setState({
-					minutes: this.state.minutes - 1,
-					seconds: 59,
-				});
-			} else {
-				this.setState({
-					seconds: this.state.seconds - 1
-				});
-			}
-
-		}, 1000)
-	}
-
-	componentDidMount() {
-		if (this.state.active === false) {
-			clearInterval(this.timerInterval);
-		} else {
-			this.startInterval();
+			seconds: this.props.seconds
 		}
 	}
 
 	render() {
 		return(
 			<div className="time">
-				{this.props.id}
 				{this.state.minutes} : {this.state.seconds}
 				<RemoveItem
 					id={this.props.id}
